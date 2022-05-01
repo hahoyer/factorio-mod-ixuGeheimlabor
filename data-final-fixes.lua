50a86cbeb210bfbb836251e9b76cf282b4142e7f
hh <hahoyer@gmail.com>
2022-05-01 22:37:17 +0200

    change: pyanodon flask recipe will create 20 flasks

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,3 +1,9 @@
-data.raw.character["character"] = data.raw.character["miku-bikini-swimsuit-skin"]
-data.raw.character["character"].name = "character"
-data.raw.character["miku-bikini-swimsuit-skin"] = nil
+if data.raw.character["miku-bikini-swimsuit-skin"] then
+    data.raw.character["character"] = data.raw.character["miku-bikini-swimsuit-skin"]
+    data.raw.character["character"].name = "character"
+    data.raw.character["miku-bikini-swimsuit-skin"] = nil
+end
+
+if data.raw.recipe.flask and data.raw.recipe.flask.results[1][1] == "flask" then
+    data.raw.recipe.flask.results[1][2] = 20
+end
